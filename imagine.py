73337b4167e7fdfa44bf1609c01a542587293d27
Claus Juhl Knudsen <earkivar@gmail.com>
2018-03-30 20:36:04 +0200

    second commit

--- imagine.py
+++ imagine.py
@@ -1,11 +1,9 @@
-from PIL import image
+from PIL import Image
 from gooey import Gooey, GooeyParser
 from pathlib import Path
 
 
-@Gooey(program_name="convert2jpg",
-       return_to_config=True,
-       header_height=60)
+@Gooey(program_name="imagine", return_to_config=True, header_height=60)
 def parse_args():
     desc = "Skalér og konvertér en eller flere billedfiler til jpg."
     input_files_msg = "Marker en eller flere filer, der skal konverteres"
@@ -29,10 +27,14 @@ def parse_args():
                         '--mheight',
                         metavar="Maksimal billedhøjde i pixels",
                         help="max_height of images in pixels")
+    # parser.add_argument('-f',
+    #                     '--format',
+    #                     metavar="Output format",
+    #                     help="Choose output-format")
     parser.add_argument('-q',
                         '--quality',
                         metavar="JPG-kvalitet",
-                        help="Choose quality of output-files. Defaults to 95%")
+                        help="Choose quality of output-files. Defaults to 75%")
     return parser.parse_args()
 
 
@@ -40,66 +42,65 @@ def get_images(dir_object):
     # dir_objectis a Path-object
     files = []
     for obj in dir_object.rglob('*.*'):
-        if obj.suffix in ['.png', '.jpg', '.jp2', '.jpeg', '.tiff', '.tif']:
+        if obj.suffix.lower() in ['.png', '.jpg', '.jp2', '.jpeg', '.tiff', '.tif']:
             files.append(str(obj))
     return files
 
 
 def read_image(file_path):
-    return image.read(file_path)
+    return image.open(file_path)
 
 
 def resize_image(file_path, max_height=None, max_width=None):
-    # https://docs.opencv.org/3.0-last-rst/modules/imgcodecs/doc/reading_and_writing_images.html?highlight=imread#cv2.imread
-    cv_image = image.read(file_path)
+    # http://pillow.readthedocs.io/en/latest/reference/Image.html#PIL.Image.Image
+    image = Image.open(file_path)
 
-    height, width = cv_image.shape[:2]
+    # https://pillow.readthedocs.io/en/latest/reference/Image.html#PIL.Image.size
+    width, height = image.size
     scaling_factor = 1
 
-    # shrink if img-dimentions are bigger than required
+    # shrink if max-dimensions are smaller than img-dimensions
     if max_height and max_height/float(height) < scaling_factor:
         scaling_factor = max_height / float(height)
 
     if max_width and max_width/float(width) < scaling_factor:
         scaling_factor = max_width / float(width)
 
-    # https://docs.opencv.org/3.0-last-rst/modules/imgproc/doc/geometric_transformations.html?highlight=resize#cv2.resize
-    resized = cv2.resize(cv_image,
-                         None,  # TODO: What is this param?
-                         fx=scaling_factor,
-                         fy=scaling_factor,
-                         interpolation=cv2.INTER_AREA)
-
-    return resized
+    # http://pillow.readthedocs.io/en/latest/reference/Image.html#PIL.Image.Image.resize 
+    return image.resize((width / scaling_factor, height / scaling_factor))
 
 
-def save_as_jpg(cv_image, filename, jpg_quality=95):
-    # https://docs.opencv.org/3.0-last-rst/modules/imgcodecs/doc/reading_and_writing_images.html?highlight=imwrite#cv2.imwrite
-    cv2.imwrite(filename,
-                cv_image,
-                [cv2.IMWRITE_JPEG_QUALITY, jpg_quality])
+def save_image(image, filename, quality):
+    image.save(filename, quality=quality, optimize=True)
 
 
 if __name__ == '__main__':
     args = parse_args()
-    print("Arguments parsed")
+    max_height = args.max_height if args.max_height else None
+    max_width = args.max_width if args.max_width else None
+    quality = 75 if not args.quality else args.quality
+
+    print("Arguments parsed", flush=True)
     images = get_images(Path(args.input_files))
-    print("Image(s) fetched")
-    print("Working with image(s)...")
+    print("Image(s) fetched", flush=True)
+    print("Working with image(s)...", flush=True)
     for img_path in images:
-        if args.get('max_height') or args.get('max_width'):
+        if max_height or max_width:
             infile = resize_image(img_path,
-                                  max_height=args.get('max_height'),
-                                  max_width=args.get('max_width'))
+                                  max_height=max_height,
+                                  max_width=max_width)
 
         else:
             infile = read_image(img_path)
 
-        if args.get('output_dir'):
+        if args.output_dir:
             filename = img_path.slice[img_path.rindex('.'):] + ".jpg"
             filepath = '/'.join([args.output_dir, filename])
         else:
             filepath = img_path
-        save_as_jpg(infile,
-                    filename=filepath,
-                    jpg_quality=args.get('quality', 95))
+
+        save_image(infile,
+                   filename=filepath,
+                   jpg_quality=quality)
+    print("Done", flush=True)
+    print("Click 'edit' to convert additional files.")
